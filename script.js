ae2185db682f47868f893585d1c2043deda5c264
Héber Lima Silva <heberlimasilva@gmail.com>
2024-02-08 11:07:49 -0300

    initial setup of azure openai api

--- script.js
+++ script.js
@@ -1,55 +1,26 @@
+require("dotenv").config();
+
+import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
+
+const client = new OpenAIClient(
+  "https://" + process.env.RESOURCE_NAME + "openai.azure.com/", 
+  new AzureKeyCredential(process.env.AZURE_API_KEY)
+);
+
 const inputQuestion = document.getElementById("inputQuestion");
 const result = document.getElementById("result");
 
-inputQuestion.addEventListener("keypress", (e) => {
-  if (inputQuestion.value && e.key === "Enter") SendQuestion();
+inputQuestion.addEventListener("keypress", async (e) => {
+  if (inputQuestion.value && e.key === "Enter") await sendPrompt();
 });
 
-const OPENAI_API_KEY = "";
-
-function SendQuestion() {
-  var sQuestion = inputQuestion.value;
-
-  fetch("https://api.openai.com/v1/completions", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + OPENAI_API_KEY,
-    },
-    body: JSON.stringify({
-      model: "text-davinci-003",
-      prompt: sQuestion,
-      max_tokens: 2048, // tamanho da resposta
-      temperature: 0.5, // criatividade na resposta
-    }),
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      if (result.value) result.value += "\n";
-
-      if (json.error?.message) {
-        result.value += `Error: ${json.error.message}`;
-      } else if (json.choices?.[0].text) {
-        var text = json.choices[0].text || "Sem resposta";
-
-        result.value += "Chat GPT: " + text;
-      }
-
-      result.scrollTop = result.scrollHeight;
-    })
-    .catch((error) => console.error("Error:", error))
-    .finally(() => {
-      inputQuestion.value = "";
-      inputQuestion.disabled = false;
-      inputQuestion.focus();
-    });
-
-  if (result.value) result.value += "\n\n\n";
-
-  result.value += `Eu: ${sQuestion}`;
-  inputQuestion.value = "Carregando...";
-  inputQuestion.disabled = true;
-
-  result.scrollTop = result.scrollHeight;
-}
+const sendPrompt = async () => {
+  
+  try {
+    var sQuestion = inputQuestion.value;
+    const response = await client.getCompletions(process.env.DEPLOYMENT_ID, [sQuestion]);
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+};
